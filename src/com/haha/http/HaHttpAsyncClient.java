dca140e48eb669801caa4f5ee0b0b4d8294d49e1
xj <xiongjin2013@126.com>
2015-08-02 13:56:54 +0800

    get http request function

--- src/com/haha/http/HaHttpAsyncClient.java
+++ src/com/haha/http/HaHttpAsyncClient.java
@@ -1,28 +1,35 @@
+
 package com.haha.http;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class HaHttpAsyncClient implements HaHttpClient {
-	
-	//a executor service object to execute the http task
-	private ExecutorService executorService;
-	
-	public HaHttpAsyncClient(){
-		init();
-	}
 
-	@Override
-	public void init() {
-		if(this.executorService == null){
-			this.executorService = Executors.newCachedThreadPool();
-		}
-	}
+    // a executor service object to execute the http task
+    private ExecutorService executorService;
 
-	@Override
-	public void get(String url, HaHttpHandler handler) throws Exception {
-		HaHttpTask task = new HaHttpGetTask(url, HaHttpCfg.DEFAULT_MAX_RETRY_COUNT, handler);
-		this.executorService.execute(task);
-	}
+    public HaHttpAsyncClient() {
+        init();
+    }
+
+    @Override
+    public void init() {
+        if (this.executorService == null) {
+            this.executorService = Executors.newCachedThreadPool();
+        }
+    }
+
+    @Override
+    public void get(String url, HaHttpHandler handler) throws Exception {
+        HaHttpTask task = new HaHttpGetTask(url, HaHttpCfg.DEFAULT_MAX_RETRY_COUNT, handler);
+        this.executorService.execute(task);
+    }
+
+    @Override
+    public void get(String url, int maxRetryCount, HaHttpHandler handler) throws Exception {
+        HaHttpTask task = new HaHttpGetTask(url, maxRetryCount, handler);
+        this.executorService.execute(task);
+    }
 
 }
--- src/com/haha/http/HaHttpClient.java
+++ src/com/haha/http/HaHttpClient.java
@@ -1,10 +1,14 @@
+
 package com.haha.http;
 
 public interface HaHttpClient {
-	
-	public void init();
-	
-	/*interfaces with retry count default 1*/
-	public void get(String url, HaHttpHandler handler) throws Exception;
+
+    public void init();
+
+    /* interfaces with retry count default 1 */
+    public void get(String url, HaHttpHandler handler) throws Exception;
+
+    /* interfaces with specified retry count */
+    public void get(String url, int maxRetryCount, HaHttpHandler handler) throws Exception;
 
 }
